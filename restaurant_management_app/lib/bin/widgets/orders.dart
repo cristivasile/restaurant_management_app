7752d76a7e989ee14d285731cc4fa05bca2050d6
Edi Podaru <edipodaru@gmail.com>
2022-05-11 21:46:20 +0300

    Added placeholder dialog in orders tab(WIP)

--- restaurant_management_app/lib/bin/widgets/orders.dart
+++ restaurant_management_app/lib/bin/widgets/orders.dart
@@ -3,8 +3,10 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:restaurant_management_app/bin/models/product_model.dart';
 import 'package:restaurant_management_app/bin/constants.dart';
+import 'package:restaurant_management_app/bin/widgets/custom_button.dart';
 
 import '../constants.dart';
+import '../entities/product_list.dart';
 import '../models/order_model.dart';
 import '../entities/order_list.dart';
 
@@ -20,21 +22,176 @@ class OrdersWidget extends StatefulWidget {
 }
 
 class _OrdersWidgetState extends State<OrdersWidget> {
+  List<ProductModel> _products = [];
+  String _dialogErrorMessage = "";
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _products = ProductList.getProductList();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    priceController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        controller: ScrollController(),
-        itemBuilder: (BuildContext context, int index) {
-          return OrderSection(
-            title: orders[index].tableId +
-                '     ' +
-                orders[index].price.toString(),
-            orderModel: orders[index],
-          );
-        },
-        itemCount: orders.length);
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(children: [
+          SizedBox(
+            width: constraints.maxWidth,
+            height: constraints.maxHeight * 9 / 10,
+            child: ListView.builder(
+                controller: ScrollController(),
+                itemBuilder: (BuildContext context, int index) {
+                  return OrderSection(
+                    title: orders[index].tableId +
+                        '     ' +
+                        orders[index].price.toString(),
+                    orderModel: orders[index],
+                  );
+                },
+                itemCount: orders.length),
+          ),
+          SizedBox(
+              width: constraints.maxWidth,
+              height: constraints.maxHeight * 1 / 10,
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                CustomButton(
+                  color: Colors.orange,
+                  size: 50,
+                  icon: const Icon(Icons.add),
+                  function: () async {
+                    //TODO - functie
+
+                    await showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) {
+                          return StatefulBuilder(builder: (context, setState) {
+                            return AlertDialog(
+                              title: const Text(
+                                "Add new order",
+                                style: TextStyle(color: mainColor),
+                              ),
+                              content: SizedBox(
+                                height: 200,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    ////
+                                    /// TODO - THIS SHOULD BE A DROPDOWN, FOLLOWED BY A BUTTON TO ADD THE PRODUCT TO THE ORDER'S LIST
+                                    /// CHECK DROPDOWN EXAMPLE IN FLOORPLAN.DART
+                                    TextField(
+                                      decoration: const InputDecoration(
+                                          hintText: "Enter product name"),
+                                      controller: nameController,
+                                    ),
+                                    /// TODO - BUTTON HERE
+                                    TextField(
+                                      decoration: const InputDecoration(
+                                          hintText: "Enter quantity"),
+                                      controller: priceController,
+                                    ),
+                                    Text(
+                                      _dialogErrorMessage,
+                                      style: const TextStyle(
+                                          color: Colors.redAccent),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                  child: const Text('Add'),
+                                  onPressed: () {
+                                    setState(() {
+                                      _dialogErrorMessage = "";
+                                    });
+
+                                    String name = nameController.text.trim();
+
+                                    if (name.length < 3) {
+                                      setState(() {
+                                        _dialogErrorMessage =
+                                            "Product name must have at least 3 characters!";
+                                      });
+                                      return;
+                                    }
+
+                                    if (name.length > 20) {
+                                      setState(() {
+                                        _dialogErrorMessage =
+                                            "Product name must have at most 20 characters!";
+                                      });
+                                      return;
+                                    }
+
+                                    for (var product
+                                        in ProductList.getProductList()) {
+                                      if (name.toLowerCase() ==
+                                          product.name.toLowerCase()) {
+                                        setState(() {
+                                          _dialogErrorMessage =
+                                              "Product name already exists!";
+                                        });
+                                        return;
+                                      }
+                                    }
+
+                                    double? price =
+                                        double.tryParse(priceController.text);
+
+                                    if (price == null || price <= 0) {
+                                      setState(() {
+                                        _dialogErrorMessage =
+                                            "Incorrect price! Must be a number higher than 0.";
+                                      });
+                                      return;
+                                    }
+
+                                    /// TODO - ADD PRODUCT TO ORDER LIST
+                                    /// CREATE ORDER
+                                  },
+                                  style:
+                                      TextButton.styleFrom(primary: mainColor),
+                                ),
+                                TextButton(
+                                  child: const Text('Cancel'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  style:
+                                      TextButton.styleFrom(primary: mainColor),
+                                ),
+                              ],
+                            );
+                          });
+                        });
+                  },
+                )
+              ]))
+        ]);
+      },
+    );
   }
 }
+//               ]),
+//             ),
+//           ],
+//         );
+//     );
+//   }
+// }
 
 //Order section/category widget
 class OrderSection extends StatefulWidget {
